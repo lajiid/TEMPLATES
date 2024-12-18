6164e76c9c120dd54ee0694024b7415c556dde85
lajiid <130635658+lajiid@users.noreply.github.com>
2024-12-18 20:46:40 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// js/config.js
+const CONFIG = {
+    GMAIL: {
+        API_KEY: 'AIzaSyArFC_bHXljlyVah-zEg_UGMS6bj7UZ7Xc',
+        CLIENT_ID: '468556246687-hcshavcp83ju66c0e9b4mm7eah20mmqe.apps.googleusercontent.com',
+        SCOPES: 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.compose'
+    }
+};
